20819c5ad3037d2842508fc4a4aa4597640676f0
Manu-Prz <perezmndzmanuel@gmail.com>
2023-02-06 13:09:40 +0100

    Añadido menú colapsable, selector de idiomas y cambios menores

--- View/Index/script/script.js
+++ View/Index/script/script.js
@@ -1,6 +1,10 @@
 window.onload = function() {
     $("#cuerpo").load("../Main/main.html");
-
+    const mqLarge  = window.matchMedia( '(min-width: 768px)' );
+    menuHandler(mqLarge);
+    
+    mqLarge.addEventListener('change', menuHandler);
+    
     let quien = document.getElementById("quien");
     let servicios = document.getElementById("servicios");
     let colabora = document.getElementById("colabora");
@@ -8,37 +12,68 @@ window.onload = function() {
     let multimedia = document.getElementById("multimedia");
 
 
-    quien.addEventListener("click", () => cargar(quien, "Quiénes somos", "../QuienesSomos/quienesSomos.html"));
-    servicios.addEventListener("click", () => cargar(servicios, "Servicios", "../Servicios/servicios.html"));
-    colabora.addEventListener("click", () => cargar(colabora, "Colabora con nosotros", "../Colabora/colabora.html"));
-    contacto.addEventListener("click", () => cargar(contacto, "Contacto", "../Contacto/contacto.html"));
-    multimedia.addEventListener("click", () => cargar(multimedia, "Multimedia", "../Multimedia/multimedia.html"));
-    
-} 
+    quien.addEventListener("click", () => cargar(quien, "../QuienesSomos/quienesSomos.html"));
+    servicios.addEventListener("click", () => cargar(servicios, "../Servicios/servicios.html"));
+    colabora.addEventListener("click", () => cargar(colabora, "../Colabora/colabora.html"));
+    contacto.addEventListener("click", () => cargar(contacto, "../Contacto/contacto.html"));
+    multimedia.addEventListener("click", () => cargar(multimedia, "../Multimedia/multimedia.html"));
 
-function cargar(elemento, texto, objetivo) {
+}
+
+// media query handler function
+function menuHandler(e) {
+    
+    if (!e.matches) {
+        $("#botonera")[0].classList.add("collapse");
+
+        if (!document.getElementById("menuDisplayer")){
+
+            let menuDisplayer = document.createElement("button");
+            let div = document.createElement("div");
+            let img = document.createElement("img");
+            
+            menuDisplayer.setAttribute("type", "button");
+            menuDisplayer.setAttribute("class", "btn btn-primary mx-4 my-5 w-100");
+            menuDisplayer.setAttribute("id", "menuDisplayer");
+            menuDisplayer.setAttribute("data-bs-toggle", "collapse");
+            menuDisplayer.setAttribute("data-bs-target", "#botonera");
+            menuDisplayer.setAttribute("aria-expanded", "false");
+            menuDisplayer.setAttribute("aria-controls", "#botonera");
+
+            img.setAttribute("src", "resources/navBar.svg");
+            menuDisplayer.append(img);
+            
+            div.setAttribute("class", "d-flex justify-content-evenly");
+            div.append(menuDisplayer);
+            
+            document.body.insertBefore(div,$("#botonera")[0]);
+        }
+        
+    } else {
+        if (document.getElementById("menuDisplayer")) {
+            document.getElementById("menuDisplayer").remove()
+        }
+    }
+    
+
+}
+
+function cargar(elemento, objetivo) {
 
     let botones = document.querySelectorAll("div.botonera button");
     for (let boton of botones) {
         if (boton != elemento) {
-            boton.classList.toggle("disabled");
-            boton.classList.toggle("opacity-25");
             
-            
+            boton.classList.remove("btn-primary");
+            boton.classList.add("btn-outline-primary");
+        } else {
+            elemento.classList.add("btn-primary");
+            elemento.classList.remove("btn-outline-primary");
+
         }
+
     }
 
-    if (elemento.innerText == "Volver") {
-        $("#cuerpo").load("../Main/main.html");
-        elemento.classList.add("btn-outline-primary");
-        elemento.classList.remove("btn-primary");
-        elemento.innerText = texto;
-
-    } else {
-        $("#cuerpo").load(objetivo);
-        elemento.classList.add("btn-primary");
-        elemento.classList.remove("btn-outline-primary");
-        elemento.innerText = "Volver";
-    }
+    $("#cuerpo").load(objetivo);
 
 }
