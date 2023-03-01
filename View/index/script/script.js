df929c84a4ea91c80311c225592b23fdbef8dd22
Manu-Prz <perezmndzmanuel@gmail.com>
2023-03-01 22:55:35 +0100

    Cabecero modificado

--- View/index/script/script.js
+++ View/index/script/script.js
@@ -1,9 +1,13 @@
 window.onload = function() {
     $("#cuerpo").load("../main/main.html");
-    const mqLarge  = window.matchMedia('(min-width: 768px)');
-    menuHandler(mqLarge);
+    //const mqLarge  = window.matchMedia('(min-width: 768px)');
+    const mqSmall  = window.matchMedia('(min-width: 862px)');
+    const mqMedium = window.matchMedia('(min-width: 1092px)');
+    menuHandler(mqSmall);
+    reArrange(mqMedium);
     
-    mqLarge.addEventListener('change', menuHandler);
+    mqSmall.addEventListener('change', menuHandler);
+    mqMedium.addEventListener('change', reArrange);
     
     let quien = document.getElementById("quien");
     let servicios = document.getElementById("servicios");
@@ -14,30 +18,38 @@ window.onload = function() {
     let queHacemos= document.getElementById("queHacemos");
     let nuestraHistoria= document.getElementById("nuestraHistoria");
     let subMenu1 = document.getElementById("subMenu1");
+    let subMenu2 = document.getElementById("subMenu2");
 
 
 	//IDEA: Lo que haría un hipotético selector de idioma sería simplemente seleccionar una variable u otra, que se concatenaría al string de la ruta enviada a la función "cargar()"
 
-    servicios.addEventListener("click", () => cargar(servicios, "../servicios/servicios.html"));
     colabora.addEventListener("click", () => cargar(colabora, "../colabora/colabora.html"));
     contacto.addEventListener("click", () => cargar(contacto, "../contacto/contacto.html"));
     multimedia.addEventListener("click", () => cargar(multimedia, "../multimedia/multimedia.html"));
     quienesSomos.addEventListener("click", () => cargar(quienesSomos, "../quienesSomos/quienesSomos.html"));
     queHacemos.addEventListener("click", () => cargar(queHacemos, "../queHacemos/queHacemos.html"));
     nuestraHistoria.addEventListener("click", () => cargar(nuestraHistoria, "../nuestraHistoria/nuestraHistoria.html"));
-    quien.addEventListener("click", rotarPuntero);
-
-    document.getElementById("logo").style.height = `${document.querySelector("#quien").offsetHeight}px`;
-    document.getElementById("logo").style.display = "inline";
+    quien.addEventListener("click", desplegarSubMenu);
+    servicios.addEventListener("click", desplegarSubMenu);
     
 }
 
 // media query handler function
 function menuHandler(e) {
     
+    let botonera = document.getElementById("botonera");
+    let divs = document.querySelectorAll("div#botonera > div");
+    let subMenu1 = document.querySelector("#subMenu1");
+    let subMenu2 = document.querySelector("#subMenu2");
+
     if (!e.matches) {
-        $("#botonera")[0].classList.add("collapse");
-        $("#botonera")[0].style.backgroundImage = "none";
+        botonera.classList.remove("d-flex");
+        botonera.classList.add("collapse", "d-flex-column");
+        botonera.style.backgroundImage = "none";
+
+        for (let div of divs) {
+            div.classList.add("mb-2");
+        }
 
         if (!document.getElementById("menuDisplayer")){
 
@@ -56,11 +68,16 @@ function menuHandler(e) {
             img.setAttribute("src", "resources/visual/navBar3.svg");
             menuDisplayer.append(img);
             
-            div.setAttribute("class", "d-flex justify-content-evenly mt-3 mb-5");
+            div.setAttribute("class", "d-flex justify-content-evenly mt-3 mb-5 w-100");
             div.setAttribute("id", "menuDisplayerContainer")
             div.append(menuDisplayer);
-            
+
+            $("#topMenu")[0].classList.add("d-flex", "flex-wrap");
+            //$("#logoContainer")[0].style.display = "none";
+            //$("#logo")[0].style.display = "none";
             $("#topMenu")[0].insertBefore(div,$("#botonera")[0]);
+
+
 
             $("#menuDisplayer")[0].addEventListener("click", () => {
                 if ($("#menuDisplayer")[0].classList.contains("collapsed")) {
@@ -69,31 +86,80 @@ function menuHandler(e) {
                 } else {
                     $("#menuDisplayer")[0].classList.add("btn-primary");
                     $("#menuDisplayer")[0].classList.remove("btn-outline-primary");
-
-                    document.getElementById("logo").style.height = `${document.querySelector("#quien").offsetHeight}px`;
-                    document.getElementById("logo").style.display = "inline";
                 }
             })
         }
+
+        botonera.insertBefore(subMenu1,$("#servicios")[0].parentNode);
+        botonera.insertBefore(subMenu2,$("#colabora")[0].parentNode);
         
     } else {
         if (document.getElementById("menuDisplayerContainer")) {
             document.getElementById("menuDisplayerContainer").remove()
         }
 
-        $("#botonera")[0].style.backgroundImage = "url('resources/visual/recta.svg')";
+        for (let div of divs) {
+            div.classList.remove("mb-2");
+        }
+
+        botonera.parentElement.lastElementChild.append(subMenu1);
+        botonera.parentElement.lastElementChild.append(subMenu2);
+
+        botonera.classList.remove("collapse", "d-flex-column");
+        botonera.classList.add("d-flex");
+        botonera.style.backgroundImage = "url('resources/visual/recta.svg')";
 
     }
     
-
 }
 
-function rotarPuntero(e) {
-    if (!e.target.classList.contains("collapsed")) {
-        document.getElementById("puntero").style.transform = "rotate(180deg)";
+function reArrange(e) {
+    let logoContainer = document.getElementById("logoContainer");
+    let logo = document.getElementById("logo");
+    let topMenu = document.getElementById("topMenu");
+
+
+    if (!e.matches) {
+        topMenu.classList.add("d-flex", "flex-wrap");
+        logoContainer.classList.remove("ps-3");
+        logoContainer.classList.remove("justify-content-end");
+        logoContainer.classList.add("justify-content-center");
+        logoContainer.classList.add("mb-3");
+        logo.style.width = "20%";
 
     } else {
-        document.getElementById("puntero").style.transform = "";
+
+        logoContainer.classList.add("ps-3");
+        logoContainer.classList.add("justify-content-end");
+        logoContainer.classList.remove("justify-content-center");
+        topMenu.classList.remove("d-flex", "flex-wrap");
+        topMenu.style.display = "grid";
+        logoContainer.classList.remove("mb-3")
+        logo.style.width = "auto";
+
+    }
+}
+
+function desplegarSubMenu(e) {
+    let subMenu = document.querySelector("div#subMenu1.show, div#subMenu2.show");
+
+    /*subMenu.forEach(element => {
+        if (element.id != e.target.getAttribute("data-bs-target").slice(1)) {
+            element.style.display = "none";
+        }
+    });*/
+
+    if (subMenu) {
+        //subMenu.classList.remove("show");
+        subMenu.classList.add("collapsing");
+        document.querySelector(`button[data-bs-target='#${subMenu.id}']`).firstElementChild.style.transform = "rotate(0deg)";
+    }
+
+    if (!e.target.classList.contains("collapsed")) {
+        e.target.firstElementChild.style.transform = "rotate(180deg)";
+
+    } else {
+        e.target.firstElementChild.style.transform = "rotate(0deg)";
     }
 }
 
@@ -103,11 +169,11 @@ function cargar(elemento, objetivo) {
     for (let boton of botones) {
         if (boton != elemento) {
             
-            boton.classList.remove("btn-primary");
-            boton.classList.add("btn-outline-primary");
+            boton.classList.add("btn-primary");
+            boton.classList.remove("btn-outline-primary");
         } else {
-            elemento.classList.add("btn-primary");
-            elemento.classList.remove("btn-outline-primary");
+            elemento.classList.remove("btn-primary");
+            elemento.classList.add("btn-outline-primary");
 
         }
 
